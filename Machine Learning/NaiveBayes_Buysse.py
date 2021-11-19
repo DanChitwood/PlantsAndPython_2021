35eb4fb9b82a94178e9ac07a4e372b806a2dd354
sfbuysse <70411966+sfbuysse@users.noreply.github.com>
2021-11-19 16:01:22 -0500

    Code for 100 genes and 24 tissue types dataset
    Updated to include only for relevant for the 100 genes with 24 tissue types data set.

--- Machine Learning/NaiveBayes_Buysse.py
+++ Machine Learning/NaiveBayes_Buysse.py
@@ -10,35 +10,29 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
 ## read in the data setting the first column as the index
-# for 3 genes and 100 genes
-dat = pd.read_csv("HRT841_3genes_test.csv", index_col=0)
-dat100 = pd.read_csv("subset_tissue.csv", index_col=0)
+# the 100 genes and 24 tissues dataset 9on github)
+dat24 = pd.read_csv("result_table_100_24_tissues.csv", index_col=0)
 
 # subset just the genes for the x data
-xdata = dat[["AT1G22630","AT1G22620","AT1G22610"]]
-xdata2 = dat100.iloc[:, 1:101]
+xdata = dat24.iloc[:, 1:101]
 
-# check the distributions of the expression data (Gaussian assumes Normal distribution)
-pd.DataFrame.hist(data = xdata2, grid = False, bins = 40, figsize = (20,20))
+# check the distributions of the expression data
+# this takes up a lot of space so only do once to get an idea
+#pd.DataFrame.hist(data = xdata, grid = False, bins = 40, figsize = (20,20))
 # so definitely not all normally distributed but that is alright for now
-# Categorical NB could be a good choice if the features are categorical
+# Categorical NB could be a good choice if the features are categorical (not what we have)
 
 
 # now get the y data that I want
 # will want to do more work here and might need to clean up a bit before my model is informative
-y_tissues = dat["Tissue"]
-y_stress = dat["Treatment"]
-# really just care wild type or mutant here?
-y_genotype = dat['Genotype']
-
-y_tissues = dat100["Tissue"]
-y_stress = dat100["Treatment"]
-y_genotype = dat100['Genotype']
+unique_tissues = dat24["Tissue"].unique()
+print(len(unique_tissues))
+y_tissues = dat24["Tissue"]
 
 # features is the x data. labels is the y data.
 # make test and train
 # starting with tissue as the label
-train, test, train_tissue, test_tissue = train_test_split(xdata2, y_tissues, test_size=0.33, random_state=42)
+train, test, train_tissue, test_tissue = train_test_split(xdata, y_tissues, test_size=0.25, random_state=42)
 
 # Initialize classifier:
 gnb = GaussianNB()
@@ -56,4 +50,4 @@ print(accuracy_score(test_tissue, predictive_tissues))
 print("GaussianNB: Number of mislabeled points out of a total %d points : %d"
       % (test.shape[0], (test_tissue != predictive_tissues).sum()))
 
-#potential issue that I am using Gaussian but I doubt we have normally distributed data
+# potential issue in choosing Gaussian. Do we have a prior expectation that the relationship of expression to tissue type would be normal?
