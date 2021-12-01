ec95810d6f274f2a1a38f32b9f834d9dc89c03ba
sfbuysse <70411966+sfbuysse@users.noreply.github.com>
2021-12-01 16:10:46 -0500

    Added annotation  notes

--- Machine Learning/NaiveBayes_Buysse.py
+++ Machine Learning/NaiveBayes_Buysse.py
@@ -9,29 +9,40 @@ import random
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-## read in the data setting the first column as the index
-# the 100 genes and 24 tissues dataset 9on github)
+## read in the data and set the first column as the index
+# this code uses the 100 genes and 24 tissues dataset (on github in the ML folder)
 dat24 = pd.read_csv("result_table_100_24_tissues.csv", index_col=0)
 
 # subset just the genes for the x data
+# the genes are our features (or predictors) so they are the x data we will be using
 xdata = dat24.iloc[:, 1:101]
 
 # check the distributions of the expression data
 # this takes up a lot of space so only do once to get an idea
+# Gaussian NB assumes the features are normally distributed, so make a histogram for each feature to see if that is the case
 #pd.DataFrame.hist(data = xdata, grid = False, bins = 40, figsize = (20,20))
 # so definitely not all normally distributed but that is alright for now
+# because we would be using a different set of features after feature selection
+
 # Categorical NB could be a good choice if the features are categorical (not what we have)
 
 
-# now get the y data that I want
+## now get the y data that we want
+# the y data is the classes we want to sort in to
 # will want to do more work here and might need to clean up a bit before my model is informative
+# depends on what a more final dataset looks like
+
+# double check there are the correct number of tissues as expected
 unique_tissues = dat24["Tissue"].unique()
 print(len(unique_tissues))
+
+#subset the tissue column to a new object
 y_tissues = dat24["Tissue"]
 
-# features is the x data. labels is the y data.
+# features is the x data. classes is the y data.
 # make test and train
 # starting with tissue as the label
+# test should be smaller than the train size
 train, test, train_tissue, test_tissue = train_test_split(xdata, y_tissues, test_size=0.25, random_state=42)
 
 # Initialize classifier:
@@ -41,16 +52,17 @@ gnb = GaussianNB()
 model = gnb.fit(train, train_tissue)
 
 # Make predictions with the classifier:
+# (predict the tissue based on the gene expression)
+# do this on the test dataset
 predictive_tissues = gnb.predict(test)
 print(predictive_tissues)
 
 # Evaluate label (subsets) accuracy:
+# (did it classify as expected?)
 print(accuracy_score(test_tissue, predictive_tissues))
 
 print("GaussianNB: Number of mislabeled points out of a total %d points : %d"
       % (test.shape[0], (test_tissue != predictive_tissues).sum()))
-
-# potential issue in choosing Gaussian. Do we have a prior expectation that the relationship of expression to tissue type would be normal?
 
 ##### from Paulo's code for testing performance #####
 # In the field of machine learning and specifically the problem of statistical classification, 
@@ -63,3 +75,8 @@ print(confusion_matrix(test_tissue, predictive_tissues))
 
 
 print(classification_report(test_tissue, predictive_tissues)) 
+
+
+### final notes on how this went:
+# not a great model fit; a different model is probably better
+# could take more time to think about a prior and if Gaussian is even appropriate
