80e2043f52504cb8797fb234e1dcca648d0bff5f
s-m-holtz <86381804+s-m-holtz@users.noreply.github.com>
2021-12-03 16:39:02 -0500

    Update same_genes.py

--- gene-exp-net/same_genes.py
+++ gene-exp-net/same_genes.py
@@ -6,7 +6,7 @@ subset_b=pd.read_csv('leafsub_filtered.csv', header=0, index_col=0)
 print("subset a shape is", subset_a.shape) #what do these look like to start
 print("subset b shape is", subset_b.shape)
 
-samegenes = subset_a.index.intersection(subset_b.index) # make variable of conserved gened between the two filtered subsets
+samegenes = subset_a.index.intersection(subset_b.index) # make variable of conserved genes between the two filtered subsets
 subset_a = subset_a[subset_a.index.isin(samegenes)] #reduce to only those genes
 subset_b = subset_b[subset_b.index.isin(samegenes)]
 print(len(samegenes), "genes conserved,\nsubset a new shape is",subset_a.shape, "subset b new shape is", subset_b.shape) # make sure thins look okay
